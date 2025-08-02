fecf37bfedef38532ba5565bee1f6798c1595753
Matheus Henrique <matheushnt06@gmail.com>
2025-08-02 09:45:38 -0300

    Faz o uso correto do template

--- js/script.js
+++ js/script.js
@@ -13,3 +13,31 @@ const criarTarefa = (descricao) => ({
   descricao,
   finalizada: false,
 });
+
+const renderizarTarefas = () => {
+  containerTarefas.innerHTML = '';
+
+  if (tarefas.length === 0) {
+    textoInfo.classList.remove('hidden');
+    containerTarefas.classList.remove('ativo');
+  } else {
+    textoInfo.classList.add('hidden');
+    containerTarefas.classList.add('ativo');
+  }
+
+  tarefas.forEach((tarefa) => {
+    const clone = templateTarefa.content.cloneNode(true);
+
+    const li = clone.querySelector('li');
+    li.setAttribute('data-id', tarefa.id);
+
+    const checkbox = clone.querySelector('.tarefa-checkbox');
+    checkbox.checked = tarefa.finalizada;
+
+    const spanDescricao = clone.querySelector('.tarefa-descricao');
+    spanDescricao.textContent = tarefa.descricao;
+    spanDescricao.title = tarefa.descricao;
+
+    containerTarefas.appendChild(clone);
+  });
+};
